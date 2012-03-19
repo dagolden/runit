02dc3b63680d81e918aed5ad063926507901b4c8
jtimberman <joshua@opscode.com>
2012-03-18 21:30:43 -0600

    [CHEF-154] - initial pass at a runit_service LWRP

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs runit and provides runit_service definition"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.15.0"
 
 recipe "runit", "Installs and configures runit"
